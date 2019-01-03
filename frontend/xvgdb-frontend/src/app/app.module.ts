1d20c2b3327cbb56eb3e6eb6b2ce06cd2fc8e107
Artq99 <amatracki@gmail.com>
2019-01-03 22:55:00 +0100

    add core module with navbar component

--- frontend/xvgdb-frontend/src/app/app.module.ts
+++ frontend/xvgdb-frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { CoreModule } from './core/core.module';
 import { GamesBrowserModule } from './games-browser/games-browser.module';
 
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    CoreModule,
     GamesBrowserModule
   ],
   bootstrap: [AppComponent]
---
+++ frontend/xvgdb-frontend/src/app/core/core.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+/**
+ * Module providing the app core components.
+ */
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    NavbarComponent
+  ],
+  exports: [
+    NavbarComponent
+  ]
+})
+export class CoreModule { }
