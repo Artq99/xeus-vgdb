9c907c25c1d2444604ff8d082da97af13f188504
Artq99 <amatracki@gmail.com>
2019-01-15 22:47:06 +0100

    add url parameters handling to games-browser.component

--- frontend/src/app/games-browser/components/games-browser.component.ts
+++ frontend/src/app/games-browser/components/games-browser.component.ts
@@ -1,27 +1,66 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 
 import { OverviewListService } from "../services/overview-list.service";
 
+/**
+ * The master component for the games browser.
+ */
 @Component({
     selector: 'app-games-browser',
     templateUrl: './games-browser.component.html'
 })
 export class GamesBrowserComponent implements OnInit {
 
-    private page: Number;
-    private maxResults: Number;
+    private page: number;
+    private maxResults: number;
     private overviewList: OverviewListItem[];
 
     constructor(
         private activatedRoute: ActivatedRoute,
+        private location: Location,
         private overviewListService: OverviewListService
     ) {}
 
     ngOnInit() {
+
+        this.resolveUrlParams();
         this.loadOverviewList();
     }
 
+    /**
+     * The method that reads the parameters from the url, hanldes the missing ones and updates the url.
+     */
+    private resolveUrlParams() {
+
+        this.activatedRoute.queryParams.subscribe(params => {
+            this.page = parseInt(params['page']);
+            this.maxResults = parseInt(params['maxResults']);
+
+            if (Number.isNaN(this.page)) {
+                this.page = 0;
+            }
+
+            if (Number.isNaN(this.maxResults)) {
+                this.maxResults = 10;
+            }
+
+            this.updateUrl();
+        });
+    }
+
+    /**
+     * The method that updates the url with the parameters.
+     */
+    private updateUrl() {
+
+        this.location.replaceState('games-browser?page=' + this.page + '&maxResults=' + this.maxResults);
+    }
+
+    /**
+     * The method that loads the list of games from the {@link OverviewListService}.
+     */
     private loadOverviewList() {
 
         this.overviewListService.getOverviewList().subscribe(resp => {
